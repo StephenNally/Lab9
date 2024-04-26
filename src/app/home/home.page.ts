deb8bd01475670e355ee122bdb74e1be7b3aef14
unknown <g00418614@atu.ie>
2024-04-26 11:48:43 +0000

    lab9 complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
+  coordinates:any ="";
+  lat:number = 0;
+  long:number = 0;
+  time:any = 0;
   constructor() {}
+
+  async getGPS(){
+    this.coordinates = await Geolocation.getCurrentPosition();
+    this.long = this.coordinates.coords.longitude;
+    this.lat = this.coordinates.coords.latitude;
+    this.time = new Date(this.coordinates.timestamp);
+  }
+
+  async openBrowser(){
+    await Browser.open({ url: 'https://steamcommunity.com/market/listings/730/AWP%20%7C%20Dragon%20Lore%20(Factory%20New)' });
+  }
 }
